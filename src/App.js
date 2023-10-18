91de5d4671dd25417e25dd7597ee81b279e17467
Tuan Trang <tuan.trang@gmail.com>
2023-10-18 09:07:32 -0500

    useReducer render state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import UseState
- from './components/UseState';
+import UseReducer from './components/UseReducer/UseReducer';
+
 function App() {
   return (
     <div className="App">
-      <UseState />
+      <UseReducer />
     </div>
   );
 }
